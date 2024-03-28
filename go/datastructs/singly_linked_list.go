c779fba0a4261ff958bb660761e39f8153596915
nanoandrew4 <nanoandrew4@gmail.com>
2024-03-28 18:18:47 +0100

    Implement singly linked list in go

---
+++ go/datastructs/singly_linked_list.go
@@ -0,0 +1,96 @@
+package datastructs
+
+import "errors"
+
+type llNode[T comparable] struct {
+	val  T
+	next *llNode[T]
+}
+
+type SinglyLinkedList[T comparable] struct {
+	head *llNode[T]
+}
+
+func (ll *SinglyLinkedList[T]) Prepend(val T) {
+	if ll.head == nil {
+		ll.head = &llNode[T]{val: val}
+		return
+	}
+	ll.head = &llNode[T]{val: val, next: ll.head}
+}
+
+func (ll *SinglyLinkedList[T]) Append(val T) {
+	if ll.head == nil {
+		ll.head = &llNode[T]{val: val}
+		return
+	}
+	node := ll.head
+	for ; node.next != nil; node = node.next {
+	}
+
+	node.next = &llNode[T]{val: val}
+}
+
+func (ll *SinglyLinkedList[T]) Get(idx int) (T, error) {
+	for i, node := 0, ll.head; node != nil; i, node = i+1, node.next {
+		if i == idx && node != nil {
+			return node.val, nil
+		}
+	}
+	var rVal T
+	return rVal, errors.New("out of bounds")
+}
+
+func (ll *SinglyLinkedList[T]) Delete(idx int) error {
+	if idx == 0 {
+		if ll.head != nil {
+			ll.head = ll.head.next
+		}
+		return nil
+	}
+
+	var prevNode *llNode[T]
+	for i, node := 0, ll.head; node != nil; i, prevNode, node = i+1, node, node.next {
+		if i == idx && node != nil && prevNode != nil {
+			prevNode.next = node.next
+			node.next = nil
+			return nil
+		}
+	}
+	return errors.New("out of bounds")
+}
+
+func (ll *SinglyLinkedList[T]) Contains(val T) bool {
+	for node := ll.head; node != nil; node = node.next {
+		if node.val == val {
+			return true
+		}
+	}
+	return false
+}
+
+func (ll *SinglyLinkedList[T]) Size() (size int) {
+	for node := ll.head; node != nil; node, size = node.next, size+1 {
+	}
+	return
+}
+
+func (ll *SinglyLinkedList[T]) Reset() {
+	ll.head = nil
+}
+
+func (ll *SinglyLinkedList[T]) Reverse() {
+	var prevNode, tmpNode *llNode[T]
+	for node := ll.head; node != nil; prevNode, node = node, tmpNode {
+		tmpNode = node.next
+		node.next = prevNode
+	}
+	ll.head = prevNode
+}
+
+func (ll *SinglyLinkedList[T]) ToArray() (outArr []T) {
+	for node := ll.head; node != nil; node = node.next {
+		outArr = append(outArr, node.val)
+	}
+	return
+}
---
+++ go/datastructs/singly_linked_list_test.go
@@ -0,0 +1,124 @@
+package datastructs
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestSinglyLinkedList_Prepend(t *testing.T) {
+	ll := SinglyLinkedList[int]{}
+	ll.Prepend(1)
+	ll.Prepend(3)
+	ll.Prepend(5)
+	if !slices.Equal(ll.ToArray(), []int{5, 3, 1}) {
+		t.Error("expected linked list to be 5, 3, 1")
+	}
+}
+
+func TestSinglyLinkedList_Append(t *testing.T) {
+	ll := SinglyLinkedList[int]{}
+	ll.Append(1)
+	ll.Append(3)
+	ll.Append(5)
+	if !slices.Equal(ll.ToArray(), []int{1, 3, 5}) {
+		t.Error("expected linked list to be 1, 3, 5")
+	}
+}
+func TestSinglyLinkedList_Contains(t *testing.T) {
+	ll := SinglyLinkedList[int]{}
+	ll.Append(1)
+	ll.Append(3)
+	ll.Append(5)
+	if !ll.Contains(3) {
+		t.Error("expected linked list to contain 3")
+	}
+	if ll.Contains(2) {
+		t.Error("expected linked list to not contain 2")
+	}
+}
+
+func TestSinglyLinkedList_Size(t *testing.T) {
+	ll := SinglyLinkedList[int]{}
+	ll.Append(1)
+	ll.Append(3)
+	ll.Append(5)
+	if ll.Size() != 3 {
+		t.Error("expected linked list size to be 3")
+	}
+
+	ll.Reset()
+	if ll.Size() != 0 {
+		t.Error("expected linked list size to be 0")
+	}
+}
+
+func TestSinglyLinkedList_Reverse(t *testing.T) {
+	ll := SinglyLinkedList[string]{}
+	ll.Prepend("one")
+	ll.Prepend("two")
+	ll.Prepend("three")
+	ll.Reverse()
+	if !slices.Equal(ll.ToArray(), []string{"one", "two", "three"}) {
+		t.Error("expected linked list to be ordered as one, two, three")
+	}
+
+	ll.Reverse()
+	ll.Reverse()
+	if !slices.Equal(ll.ToArray(), []string{"one", "two", "three"}) {
+		t.Error("expected linked list to be ordered as one, two, three")
+	}
+}
+
+func TestSinglyLinkedList_Get(t *testing.T) {
+	ll := SinglyLinkedList[string]{}
+	ll.Prepend("one")
+	ll.Prepend("two")
+	ll.Prepend("three")
+	val, err := ll.Get(0)
+	if err != nil || val != "three" {
+		t.Error("expected retrieved value to be \"one\"")
+	}
+	val, err = ll.Get(-1)
+	if err == nil || val != "" {
+		t.Error("expected error and default value for searching with negative index")
+	}
+
+	val, err = ll.Get(10)
+	if err == nil || val != "" {
+		t.Error("expected error and default value for searching out of bounds")
+	}
+}
+
+func TestSinglyLinkedList_Delete(t *testing.T) {
+	ll := SinglyLinkedList[int]{}
+	ll.Append(1)
+	ll.Append(2)
+	ll.Append(3)
+	ll.Append(4)
+	ll.Append(5)
+
+	if ll.Delete(0) != nil {
+		t.Error("error deleting first element of list")
+	}
+	if !slices.Equal(ll.ToArray(), []int{2, 3, 4, 5}) {
+		t.Error("expected 1 to be deleted from list")
+	}
+
+	if ll.Delete(3) != nil {
+		t.Error("error deleting last element of list")
+	}
+	if !slices.Equal(ll.ToArray(), []int{2, 3, 4}) {
+		t.Error("expected 5 to be deleted from list")
+	}
+
+	if ll.Delete(1) != nil {
+		t.Error("error deleting middle element of list")
+	}
+	if !slices.Equal(ll.ToArray(), []int{2, 4}) {
+		t.Error("expected 3 to be deleted from list")
+	}
+
+	if ll.Delete(-1) == nil {
+		t.Error("expected error deleting at negative index")
+	}
+}
